17446904ce3c56abbb40145dff2a7cf52f9ccdfa
Tejas Ashok Bhujade <tejas.bhujade24@sanjivani.edu.in>
2024-09-24 23:07:03 +0530

    Add files via upload
    It is an Program to demonstrate the Logical Operators.

---
+++ 14_logical operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a = 5, b = 3;
+
+// Logical AND operator
+cout << "a && b is " << (a && b) << endl;
+
+// Logical OR operator
+cout << "a || b is " << (a || b) << endl;
+
+// Logical NOT operator
+cout << "!b is " << (!b) << endl;
+
+return 0;
+}
